b5f9dfcc7bf7fa036a635b90541f4a750248d5b4
ACERobotics <>
2024-03-01 09:09:11 -0800

    first commit

---
+++ src/main/java/frc/robot/autos/simpleAuto.java
@@ -0,0 +1,24 @@
+package frc.robot.autos;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.Swerve;
+
+public class simpleAuto  extends SequentialCommandGroup {
+    public simpleAuto(Swerve s_Swerve){
+    addRequirements(s_Swerve);
+    addCommands(
+        new InstantCommand(() -> s_Swerve.setHeading(Rotation2d.fromDegrees(180))),
+        new InstantCommand(() -> s_Swerve.drive(new Translation2d(1, 0), 0, true, true)),
+        new WaitCommand(1),
+        new InstantCommand(() -> s_Swerve.drive(new Translation2d(.5, 0), 0, true, true)),
+        new WaitCommand(2),
+        new InstantCommand(() -> s_Swerve.drive(new Translation2d(0, 0), 0, true, true))
+    );
+    }
+    
+}
